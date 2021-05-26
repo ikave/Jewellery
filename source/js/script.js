d77393c4c7c15910736cefa809b942607e1deaf7
ikave <shiftgear@mail.ru>
2021-05-26 20:59:01 +0300

    Оживляет слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -20,12 +20,38 @@ const swiper = new Swiper('.swiper-container', {
   centeredSlides: false,
   spaceBetween: 30,
   centeredSlidesBounds: true,
+  loop: true,
 
   breakpoints: {
+    320: {
+      pagination: {
+        type: 'fraction',
+        clickable: false,
+        renderFraction: function (currentClass, totalClass) {
+          return '<span class="' + currentClass + '"></span>' +
+                  ' of ' +
+                  '<span class="' + totalClass + '"></span>';
+        }
+      },
+    },
     1024: {
       slidesPerView: 4,
       slidesPerGroup: 4,
     },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<button class="' + className + '">' + (index + 1) + '</button>';
+    }
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 
 
