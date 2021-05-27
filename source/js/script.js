c3db3bdbd0c4bc29828d0e5a8270f4e30f410a11
ikave <shiftgear@mail.ru>
2021-05-27 13:45:53 +0300

    Подгоняет макет под ПП на десктопе

--- source/js/script.js
+++ source/js/script.js
@@ -1,67 +1,83 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(() => {
+  const pageHeader = document.querySelector('.page-header');
+  const headerToggle = document.querySelector('.page-header__toggle');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+  if (pageHeader) {
+    pageHeader.classList.remove('page-header--nojs');
   }
-});
 
-const swiper = new Swiper('.swiper-container', {
-  slidesPerGroup: 2,
-  slidesPerView: 2,
-  centeredSlides: false,
-  spaceBetween: 30,
-  centeredSlidesBounds: true,
-  loop: true,
+  if (headerToggle) {
+    headerToggle.addEventListener('click', function () {
+      if (pageHeader.classList.contains('page-header--closed')) {
+        pageHeader.classList.remove('page-header--closed');
+        pageHeader.classList.add('page-header--opened');
+        document.body.classList.add('hidden');
+      } else {
+        pageHeader.classList.add('page-header--closed');
+        pageHeader.classList.remove('page-header--opened');
+        document.body.classList.remove('hidden');
+      }
+    });
+  }
+})();
 
-  breakpoints: {
-    320: {
-      pagination: {
-        type: 'fraction',
-        clickable: false,
-        renderFraction: function (currentClass, totalClass) {
-          return '<span class="' + currentClass + '"></span>' +
-                  ' of ' +
-                  '<span class="' + totalClass + '"></span>';
-        }
+(() => {
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerGroup: 2,
+    slidesPerView: 2,
+    centeredSlides: false,
+    spaceBetween: 30,
+    centeredSlidesBounds: true,
+    loop: true,
+
+    breakpoints: {
+      320: {
+        pagination: {
+          type: 'fraction',
+          clickable: false,
+          renderFraction: function (currentClass, totalClass) {
+            return '<span class="' + currentClass + '"></span>' +
+                    ' of ' +
+                    '<span class="' + totalClass + '"></span>';
+          }
+        },
+      },
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        allowTouchMove: false,
       },
     },
-    1024: {
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      allowTouchMove: false,
+
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<button class="' + className + '">' + (index + 1) + '</button>';
+      }
     },
-  },
 
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<button class="' + className + '">' + (index + 1) + '</button>';
-    }
-  },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+})();
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+(() => {
+  const faqAccordion = document.querySelectorAll('.faq__item');
+  const faqButtons = document.querySelectorAll('.faq__toggle');
 
-const faqAccordion = document.querySelectorAll('.faq__item');
-const faqButtons = document.querySelectorAll('.faq__toggle');
+  // if (faqAccordion) {
+    faqAccordion.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        item.classList.toggle('faq__item--opened');
+        faqButtons[index].classList.toggle('faq__toggle--opened');
+      })
+    })
+  // }
 
-faqAccordion.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    item.classList.toggle('faq__item--opened');
-    faqButtons[index].classList.toggle('faq__toggle--opened');
-  })
-})
+})();
